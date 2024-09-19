7aa56ac52c9ec30b570d148f219b1ed5a12eccf3
JeroEngel <85593415+JeroEngel@users.noreply.github.com>
2024-09-19 17:20:28 -0300

    first setup commit

---
+++ snippets/src/test/kotlin/com/github/teamverdeingsis/snippets/SnippetsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.teamverdeingsis.snippets
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetsServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
