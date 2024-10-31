11f0860c6920538d0670d2f78679100fa1c5a62a
Your GitHub Username <your.email@example.com>
2024-10-31 10:51:07 -0700

    la mauski

--- src/main/kotlin/com/github/teamverdeingsis/snippets/security/AudienceValidator.kt
+++ src/main/kotlin/com/github/teamverdeingsis/snippets/security/AudienceValidator.kt
@@ -1,17 +1,17 @@
 package com.github.teamverdeingsis.snippets.security
 
-import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2Error
-import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
+import org.springframework.security.oauth2.jwt.Jwt
 
 class AudienceValidator(private val audience: String) : OAuth2TokenValidator<Jwt> {
-    override fun validate(jwt: Jwt): OAuth2TokenValidatorResult {
-        val audiences = jwt.audience
-        return if (audiences.contains(audience)) {
+    override fun validate(token: Jwt): OAuth2TokenValidatorResult {
+        val error = OAuth2Error("invalid_token", "The required audience is missing", null)
+        return if (token.audience.contains(audience)) {
             OAuth2TokenValidatorResult.success()
         } else {
-            OAuth2TokenValidatorResult.failure(OAuth2Error("invalid_token", "The required audience is missing", null))
+            OAuth2TokenValidatorResult.failure(error)
         }
     }
 }
--- src/main/kotlin/com/github/teamverdeingsis/snippets/security/OAuth2ResourceServerSecurityConfiguration.kt
+++ src/main/kotlin/com/github/teamverdeingsis/snippets/security/OAuth2ResourceServerSecurityConfiguration.kt
@@ -1,57 +1,54 @@
 package com.github.teamverdeingsis.snippets.security
-
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
+import org.springframework.http.HttpMethod.GET
+import org.springframework.http.HttpMethod.POST
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
-import org.springframework.security.config.web.server.ServerHttpSecurity.http
-import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.JwtValidators
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
-class OAuth2ResourceServerSecurityConfiguration(
+class SecurityConfiguration(
     @Value("\${auth0.audience}") val audience: String,
-    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}") val issuer: String
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    val issuer: String,
 ) {
-
-//    @Bean
-//    fun filterChain(http: HttpSecurity): SecurityFilterChain {
-//        http
-//            .authorizeHttpRequests {
-//                it
-//                    .requestMatchers(HttpMethod.GET, "/api/snippets").hasAuthority("SCOPE_read:snippets")
-//                    .requestMatchers(HttpMethod.POST, "/api/snippets").hasAuthority("SCOPE_write:snippets")
-//                    .requestMatchers(HttpMethod.GET, "/api/snippets/hello").permitAll()
-//                    .anyRequest().authenticated()
-//            }
-//            .oauth2ResourceServer {
-//                it.jwt()
-//            }
-//
-//        println("Security configuration: ${http}")
-//
-//        return http.build()
-//    }
-
-@Bean
-fun filterChain(http: HttpSecurity): SecurityFilterChain {
-    http
-        .authorizeHttpRequests {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests {
             it
+                .requestMatchers("/").permitAll()
+                .requestMatchers(GET, "/snippet").hasAuthority("SCOPE_read:snippets")
+                .requestMatchers(GET, "/snippet/*").hasAuthority("SCOPE_read:snippets")
+                .requestMatchers(POST, "/snippet").hasAuthority("SCOPE_write:snippets")
+                .requestMatchers("/swagger-ui.html").permitAll()
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/docs/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
         }
-    println("Security configuration: ${http}")
-    return http.build()
-}
+            .oauth2ResourceServer { it.jwt(withDefaults()) }
+            .cors {
+                it.configurationSource(corsConfigurationSource())
+            }
+            .csrf {
+                it.disable()
+            }
+        return http.build()
+    }
+
     @Bean
     fun jwtDecoder(): JwtDecoder {
         val jwtDecoder = NimbusJwtDecoder.withIssuerLocation(issuer).build()
@@ -59,15 +56,21 @@ fun filterChain(http: HttpSecurity): SecurityFilterChain {
         val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(issuer)
         val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
         jwtDecoder.setJwtValidator(withAudience)
-
-        // Debugging JWT claims
-        jwtDecoder.setJwtValidator { jwt ->
-            println("JWT Audience: ${jwt.audience}")
-            println("JWT Claims: ${jwt.claims}")
-            DelegatingOAuth2TokenValidator(withIssuer, audienceValidator).validate(jwt)
-        }
-
         return jwtDecoder
     }
 
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config =
+            CorsConfiguration().apply {
+                allowCredentials = false
+                addAllowedOrigin("*") // Adjust this based on your security requirements
+                addAllowedOriginPattern("*")
+                addAllowedHeader("*")
+                addAllowedMethod("*")
+            }
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        return source
+    }
 }
